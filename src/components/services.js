8e1eb43852c90604f92bae72e9fae08fc4d24247
kate-joyce <katejoyce@live.co.uk>
2024-03-05 13:25:17 +0000

    updated styles services component

--- src/components/services.js
+++ src/components/services.js
@@ -2,10 +2,16 @@ function Services() {
   return (
     <section id="services">
       <h2>Services</h2>
-      <h3>Virtual Tutoring</h3>
-      <p>Engaging online sessions tailored to individual student needs.</p> 
-      <h3>Face to Face Tutoring</h3>
-      <p>Personalized, in-person instruction to support academic growth.</p> 
+      <div className="container flex">
+        <div>
+          <h3>Virtual Tutoring</h3>
+          <p>Engaging online sessions tailored to individual student needs.</p> 
+        </div>
+        <div>
+          <h3>Face to Face Tutoring</h3>
+          <p>Personalized, in-person instruction to support academic growth.</p> 
+        </div>   
+      </div>  
     </section>
   );
 }
